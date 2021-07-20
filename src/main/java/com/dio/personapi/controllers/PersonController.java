253d262fcf9a136d915889dff7cc957a6278671d
AugustoCesar <augustocesar.udi@gmail.com>
2021-07-20 20:59:40 -0300

    PersonDTO: mapper, request, response

--- src/main/java/com/dio/personapi/controllers/PersonController.java
+++ src/main/java/com/dio/personapi/controllers/PersonController.java
@@ -1,16 +1,9 @@
 package com.dio.personapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-
-    @GetMapping
-    public String getTestingAPI(){
-        return "API test is working!!!";
-    }
 
 }
